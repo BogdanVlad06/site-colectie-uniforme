626b6e5194ed001f2f8e09a830d875cff1036407
GBVlad <guraubogdanvlad@gmail.com>
2024-11-28 22:24:54 +0200

    Event. 1 Dec Pop-up!

--- sketch.js
+++ sketch.js
@@ -4,3 +4,22 @@ function toggleDescription(imageElement) {
       description.classList.toggle('show');
   }
 }
+// Display the modal when the page loads
+// Check if the modal has been shown before
+window.onload = function () {
+  const modal = document.getElementById("eventModal");
+  const modalShown = sessionStorage.getItem("modalShown");
+
+  if (!modalShown) {
+      // Show the modal
+      modal.style.display = "block";
+      // Mark the modal as shown in sessionStorage
+      sessionStorage.setItem("modalShown", "true");
+  }
+};
+
+// Close the modal
+function closeModal() {
+  const modal = document.getElementById("eventModal");
+  modal.style.display = "none";
+}
