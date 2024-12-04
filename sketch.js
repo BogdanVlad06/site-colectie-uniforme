8eda361765ef75809103d7b877c5b246546f6ced
GBVlad <guraubogdanvlad@gmail.com>
2024-12-04 10:46:03 +0200

    Made the modal event Pop-up appear only 2 weeks before Romania's National Day

--- sketch.js
+++ sketch.js
@@ -7,11 +7,16 @@ function toggleDescription(imageElement) {
 // Check if the modal has been shown before Display the modal when the page loads
 window.onload = function () {
   console.log("Modal logic executed");
-
+  
   const modal = document.getElementById("eventModal");
   const modalShown = sessionStorage.getItem("modalShown");
+  
+  const today = new Date();
+  const decemberFirst = new Date(today.getFullYear(), 11, 1);  // Month is 0-based (0 = January, 11 = December)
+  const twoWeeksBefore = new Date(decemberFirst);
 
-  if (!modalShown) {
+  twoWeeksBefore.setDate(decemberFirst.getDate() - 14); // Subtract 14 days
+  if (today >= twoWeeksBefore && today < decemberFirst && !modalShown) {
       // Show the modal
       modal.style.display = "block";
       // Mark the modal as shown in sessionStorage
